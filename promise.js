696ee8384606da58ca7ab7d9457f862e02f7caaa
rudywn <rudy.hrm1@gmail.com>
2022-06-12 20:33:36 +0700

    feat: challenge-07-rudywn

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let ixx = await promiseTheaterIXX();
+  let vgc = await promiseTheaterVGC();
+  let gabungan = ixx.concat(vgc);
+  let hasilGabungan = 0;
+  gabungan.map((ixx) => ixx.hasil === emosi && (hasilGabungan += 1));
+  return hasilGabungan;
+};
 
 module.exports = {
   promiseOutput,
